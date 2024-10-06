5e06d12b4e320fc7dae2cb2e30853b7151a3d7a2
gio10herrera <herrerafontalvo@outlook.com>
2024-10-06 01:45:11 -0500

    correccion al angulo para mostrar solo 2 decimales, decoracion al texto

--- src/triangulosapp/Triangles.java
+++ src/triangulosapp/Triangles.java
@@ -42,6 +42,7 @@ public class Triangles {
 
     private static void lawOfCosines() {
         angleA = Math.toDegrees(Math.acos((Math.pow(a,2)-Math.pow(b,2)-Math.pow(c,2)) / (-2*b*c)));
+        angleA = Math.round(angleA * 100) / 100.0;
     }
 
     private static void areaOfTheTriangle() {
@@ -49,12 +50,12 @@ public class Triangles {
     }
 
     private static void showResults() {
-        String result = "\t\t\t\tTRIANGULOS\n\nSide a: " + a + "\t\tSide b: " + b + "\t\tSide c: " + c + "\n\n\t\t\t\t\tHeight (h)\n\n\nThe triangle is: " + typeOfTriangle + "\nThe degree of angle A is: " + angleA + "\nThe area of the triangle is: " + areaTriangle;
+        String result = "\t\tTRIANGLES\n\nSide a: " + a + "\t\tSide b: " + b + "\t\tSide c: " + c + "\n\n\t\tHeight (h): " + h + "\n\n\nThe triangle is: " + typeOfTriangle + "\nThe degree of angle A is: " + angleA + " degrees" + "\nThe area of the triangle is: " + areaTriangle;
         //JTextArea Configuration
         JTextArea textArea = new JTextArea();
         textArea.setEditable(false);
-        textArea.setSize(720, 400);
-        textArea.setBorder(new EmptyBorder(5, 5, 5, 5));
+        textArea.setSize(520, 400);
+        textArea.setBorder(new EmptyBorder(5, 45, 5, 5));
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         textArea.setText(result);
